4a42342f7bbf175f210ce62efd67a600dc81eabe
Albert Jessurum <ajessu@gmail.com>
2012-12-03 18:38:08 +0100

    Remove Factory build through container

--- JmikolaAutoLoginBundle.php
+++ JmikolaAutoLoginBundle.php
@@ -2,20 +2,9 @@
 
 namespace Jmikola\AutoLoginBundle;
 
-use Jmikola\AutoLoginBundle\Security\AutoLoginFactory;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class JmikolaAutoLoginBundle extends Bundle
 {
-    /**
-     * @see Symfony\Component\HttpKernel\Bundle\Bundle::build()
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $extension = $container->getExtension('security');
-        $extension->addSecurityListenerFactory(new AutoLoginFactory());
-    }
 }
